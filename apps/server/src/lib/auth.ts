253203aecd4dd19158400f6985bbc7da5c775c74
Ayush <codesbyayush@gmail.com>
2025-07-13 11:35:51 +0530

    auth cookie cache too many db calls

--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -5,34 +5,42 @@ import { getDb } from "../db";
 import * as schema from "../db/schema";
 
 export function getAuth(env: Record<string, string>): unknown {
-	return betterAuth({
-		baseURL: env.BETTER_AUTH_URL as string,
-		database: drizzleAdapter(getDb(env as { DATABASE_URL: string }), {
-			provider: "pg",
-			schema: schema,
-		}),
-		trustedOrigins: ["*"],
-		emailAndPassword: {
-			enabled: true,
-		},
-		advanced: {
-			crossSubDomainCookies: {
-				enabled: true,
-				domain: env.COOKIE_DOMAIN,
-			},
-			defaultCookieAttributes: {
-				secure: true,
-				httpOnly: true,
-				sameSite: "none",
-				// partitioned: true, // New browser standards will mandate this for foreign cookies
-			},
-		},
-		socialProviders: {
-			google: {
-				clientId: (env.GOOGLE_CLIENT_ID as string) || "",
-				clientSecret: (env.GOOGLE_CLIENT_SECRET as string) || "",
-			},
-		},
-		plugins: [admin(), organization()],
-	});
+  return betterAuth({
+    baseURL: env.BETTER_AUTH_URL as string,
+    database: drizzleAdapter(getDb(env as { DATABASE_URL: string }), {
+      provider: "pg",
+      schema: schema,
+    }),
+    session: {
+      cookieCache: {
+        enabled: true,
+        maxAge: 5 * 60,
+      },
+      expiresIn: 60 * 60 * 24 * 28,
+      updateAge: 60 * 60 * 24 * 7,
+    },
+    trustedOrigins: ["*"],
+    emailAndPassword: {
+      enabled: true,
+    },
+    advanced: {
+      crossSubDomainCookies: {
+        enabled: true,
+        domain: env.COOKIE_DOMAIN,
+      },
+      defaultCookieAttributes: {
+        secure: true,
+        httpOnly: true,
+        sameSite: "none",
+        // partitioned: true, // Don't know what it is for yet read something but didn't understand shit
+      },
+    },
+    socialProviders: {
+      google: {
+        clientId: (env.GOOGLE_CLIENT_ID as string) || "",
+        clientSecret: (env.GOOGLE_CLIENT_SECRET as string) || "",
+      },
+    },
+    plugins: [admin(), organization()],
+  });
 }
