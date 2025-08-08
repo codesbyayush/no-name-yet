b2d06a47fb04dda3cea2508a6c6d2013822e513d
Ayush <codesbyayush@gmail.com>
2025-08-09 03:27:38 +0530

    again db problem have to check - somewhere drizzle conf is messed up

--- apps/server/src/db/migrations/0002_clammy_tiger_shark.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "tags" DROP CONSTRAINT "tags_organization_id_name_unique";--> statement-breakpoint
-ALTER TABLE "tags" ADD CONSTRAINT "tags_organization_id_type_unique" UNIQUE("organization_id","type");
