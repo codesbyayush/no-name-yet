69296cf99ebeb1436373611408b2a56a771bbda5
Ayush <codesbyayush@gmail.com>
2025-08-15 19:52:43 +0530

    db on halt landing page revamp

--- apps/public/src/routes/_landing/index.tsx
+++ apps/public/src/routes/_landing/index.tsx
@@ -1,6 +1,15 @@
+import { HeroHeader } from "@/components/landing/header";
+import HeroSection from "@/components/landing/hero-section";
 import { createFileRoute } from "@tanstack/react-router";
-import LandingPage from "../../components/landing";
 
 export const Route = createFileRoute("/_landing/")({
-  component: () => <LandingPage />,
+	component: () => <LandingPage />,
 });
+
+const LandingPage = () => {
+	return (
+		<>
+			<HeroSection />
+		</>
+	);
+};
