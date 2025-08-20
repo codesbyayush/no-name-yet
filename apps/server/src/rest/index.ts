71b6d03733ace6c394fc05287f06f99a79ed0d05
Ayush <codesbyayush@gmail.com>
2025-08-20 19:14:07 +0530

    github-integration: auto redirect after org linking setup

--- apps/server/src/rest/index.ts
+++ apps/server/src/rest/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from "hono";
+import githubSetupRouter from "./github-setup";
 import publicApiRouter from "./public";
 
 const v1Router = new Hono();
 
 v1Router.route("/public", publicApiRouter);
+v1Router.route("/github", githubSetupRouter);
 
 v1Router.post("/post", async (c) => {
 	const body = await c.req.json();
