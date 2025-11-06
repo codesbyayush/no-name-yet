8e8a58cb7908ca3d571c82d4a42624bfd9cfc06e
Ayush <codesbyayush@gmail.com>
2025-11-07 03:25:24 +0530

    refactor: biome suggestion and missing type

--- apps/public/src/routes/auth.tsx
+++ apps/public/src/routes/auth.tsx
@@ -30,7 +30,7 @@ function RouteComponent() {
   }
 
   return (
-    <div className='mx-auto flex min-h-screen w-full items-center justify-center bg-gradient-to-b from-card via-90% to-black py-8'>
+    <div className='mx-auto flex min-h-screen w-full items-center justify-center bg-linear-to-b from-card via-90% to-black py-8'>
       <div className='mx-auto flex w-full max-w-2xs flex-col items-center justify-center'>
         <p className='relative top-2 rounded-full bg-primary-foreground/50 px-3.5 py-2 font-bold text-4xl'>
           A
